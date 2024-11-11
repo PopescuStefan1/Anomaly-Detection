d0160d89c762e4ededee7376c7cf7494c7e5026d
PopescuStefan1 <stefan.ro95@gmail.com>
2024-11-11 19:10:39 +0200

    Fix training data centers

--- Anomaly-Detection-Lab3/ex1.py
+++ Anomaly-Detection-Lab3/ex1.py
@@ -2,7 +2,7 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 import numpy as np
 
-X, y = make_blobs(n_samples=500, n_features=2)
+X, y = make_blobs(n_samples=500, centers=[[0, 0]])
 
 plt.scatter(X[:, 0], X[:, 1])
 plt.show()
